e6a6b4ef042ee100fd877188613c993d0e7b5a0c
Nisarg Mewada <nisargmewada@gmail.com>
2024-01-05 11:23:36 +0100

    Adding Profile Photo and animation

---
+++ components/intro.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+import { motion } from "framer-motion";
+
+export default function Intro() {
+  return (
+    <section>
+      <div className="flex items-center justify-center">
+        <div className="relative">
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "tween",
+              duration: 0.3,
+            }}
+          >
+            <Image
+              src="/photo.jpg"
+              alt="Nisarg Potrait"
+              width="150"
+              height="150"
+              quality="95"
+              priority={true}
+              className="h-30 w-30 rounded-full border-[0.35rem] object-cover border-white shadow-xl"
+            />
+          </motion.div>
+          <motion.span
+            className="absolute bottom-0 right-0 text-5xl"
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 125,
+              delay: 0.1,
+              duration: 0.7,
+            }}
+          >
+            👋
+          </motion.span>
+        </div>
+      </div>
+    </section>
+  );
+}
